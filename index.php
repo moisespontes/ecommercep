67759ec543dcc591bcd82e50ccb14b1541480997
Moises Pontes <sesiom_assis@hotmail.com>
2020-05-10 20:00:07 -0300

    Inicialização das rotas

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php 
 
 require_once("vendor/autoload.php");
+use Slim\Slim;
+use Hcode\Page;
+use Hcode\PageAdmin;
 
-$app = new \Slim\Slim();
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-    $sql =  new Hcode\DB\Sql();
-    $results  = $sql->select("SELECT * FROM tb_users");
-    //ola
-    echo json_encode($results);
+    $page =  new Page();
+    $page->setTpl("index");
 
 });
 
 $app->run();
-
